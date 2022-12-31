7b7be76536b22772adb5d9209a4a7f7b96cf5071
Brecht Carlier <brecht@intigriti.com>
2022-12-31 10:40:22 +0100

    EF6 Example

---
+++ EF7.MigrationBug/Migrations/20221231093755_Init.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF7.MigrationBug.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Table",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    Discriminator = table.Column<string>(type: "text", nullable: false),
+                    ParentUserId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Table", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Table");
+        }
+    }
+}
---
+++ EF7.MigrationBug/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello World");
+
+class DbCtx : DbContext
+{
+    public DbSet<Root> Table { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        const string connectionsString =
+            "Host=localhost;Port=5432;User Id=postgres;Password=XXXXXX;Database=migration_bug";
+
+        optionsBuilder.UseNpgsql(connectionsString, o => { o.SetPostgresVersion(new Version("14.4")); });
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Root>();
+        modelBuilder.Entity<Child>();
+        modelBuilder.Entity<GrandChild>();
+    }
+}
+
+abstract class Root
+{
+    public Guid Id { get; set; }
+    public long CreatedAt { get; set; }
+}
+
+class Child : Root
+{
+}
+
+class GrandChild : Child
+{
+    public Guid ParentUserId { get; init; }
+}
